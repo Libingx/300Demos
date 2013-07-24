5708255617d73648457a96568bb094e38ff678c1
libingx <libingx006@163.com>
2013-07-24 15:34:59 +0800

    Initial commit

---
+++ Day4/src/Hello.java
@@ -0,0 +1,10 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+/**
+ * Created by sun on 13-7-24.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
