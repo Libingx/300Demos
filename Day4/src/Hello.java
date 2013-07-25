34d2f0470e77d9b9aa2ffe4f96c5d003cfd67517
Libingx <libingx006@163.com>
2013-07-25 14:36:10 +0800

    改变输出流，windows下盘符应该这么写"D:\\Download\\300Demos\\Day4\\src\\log.txt" Hello类中有各种方法 新增值传递和引用传递

--- Day4/src/Hello.java
+++ Day4/src/Hello.java
@@ -1,3 +1,4 @@
+import com.sun.xml.internal.ws.handler.ClientSOAPHandlerTube;
 
 /**
  * Created by sun on 13-7-24.
@@ -22,5 +23,35 @@ public class Hello {
             System.err.println("密码错误！");
         } else
             System.out.println("欢迎您" + username + "先生");
+    }
+
+    public void valuePass(String  str){
+        /* System.out.println("请输入一个数,i：");
+        int i = scanner.nextInt();
+        System.out.println("您输入的数为,i："+i);
+        hello.valuePass(i);
+        System.out.println("您输入的i经过方法调用后,i = "+i);
+        System.out.println("传入i，i = "+i);
+        i++;
+        System.out.println("i自增后，i = "+i);
+        */
+
+        System.out.println("传入str，str = "+str);
+        str += "您好！";
+        System.out.println("i自增后，str = "+str);
+    }
+
+    public void refPass(Integer i){
+        System.out.println("您传入"+i);
+        i++;
+        System.out.println("改变您传入的东西"+i);
+    }
+    public void finalRelatedTest(){
+        final StringBuffer stringBuffer = new StringBuffer("sun");
+        System.out.println("final 引用不能变，其值能变，stringBuffer为"+stringBuffer+"\r\n"+"append(\"sean\")后");
+        stringBuffer.append("sean");
+        System.out.println(stringBuffer+"当你试图改变这个引用时");
+        stringBuffer = new StringBuffer("wen");
+
     }
 }
--- Day4/src/TernaryOperator.java
+++ Day4/src/TernaryOperator.java
@@ -6,20 +6,7 @@ import java.util.Scanner;
 public class TernaryOperator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("请输入一个数字：");
-//        int num = scanner.nextInt();
-//        String comment = num%2==0?"是一个偶数":"是一个奇数";
-//        System.out.println(comment);
         Hello hello = new Hello();
-//        System.out.println("请输入一个年份：");
-//        int year = scanner.nextInt();
-//        hello.isALeapYear(year);
-        System.out.println("请输入用户名：");
-        String username = scanner.nextLine();
-        System.out.println("请输入密码：");
-        String password = scanner.nextLine();
-
-        hello.checkLogin(username,password);
-
+        hello.finalRelatedTest();
     }
 }
